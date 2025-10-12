847740139f8e0b099a470188a59f68352902e97c
BHHristov <bhristov16@gmail.com>
2025-10-12 17:41:26 +0300

    Adding CSS Homework

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
 console.log("Hello! My first ts code.")
 
+let x:number= 5;
+
+console.log(x++);
+console.log(--x);
+
+let browser:string="firefox";
+
+if(browser==="mozilla" || browser=== "firefox")
+{
+    console.log("yes")
+}
