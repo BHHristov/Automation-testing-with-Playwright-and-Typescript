1e8ff515263877b0c3a3a12a7dc2ef5fdd5f72cd
BHHristov <bhristov16@gmail.com>
2025-10-14 21:03:25 +0300

    Add basic-types.ts

---
+++ basic-types.ts
@@ -0,0 +1,29 @@
+let num: number = 5
+num = 10;
+num = 15;
+
+const welcomeMsg: string = "Wlcome";
+
+let message: string = "Hello, Peter!";
+let messageLenght: number = message.length;
+console.log(messageLenght);
+
+
+//string interpolation
+
+let welcomeMessageDetails: string = `Text message length is ${messageLenght}`
+console.log(welcomeMessageDetails);
+
+let numOne: number = 10;
+let numTwo: number = 5;
+
+let add: number = numOne + numTwo;
+let substract: number = numOne - numTwo;
+let multiply: number = numOne * numOne;
+let devide: number = numOne / numTwo;
+
+//boolean
+
+let areEqual: boolean = numOne === numTwo;
+console.log("areEqual", areEqual);
+ 
