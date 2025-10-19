6d5074ff842de464a672afac4cbbe253282171de
BHHristov <bhristov16@gmail.com>
2025-10-19 16:04:07 +0300

    Add functions

--- functions.ts
+++ functions.ts
@@ -9,3 +9,30 @@ function devideNumbers(a: number, b: number): number{
 }
  
 console.log(devideNumbers(10,2));
+
+const sumNums = (a: number, b: number) => a + b;
+console.log(sumNums(3,5));
+
+
+function greeting(name: string, age?: number){
+    if(age){
+        console.log(`${name} is ${age} years old.`);
+         
+    }else{
+        console.log(`Hello, ${name}!`);
+       
+    }
+
+}
+
+greeting("Ivan");
+greeting("Pesho", 34);
+
+function substractNums(a: number = 10, b:number = 5){
+    console.log(a - b);
+    
+}
+substractNums();
+substractNums(25);
+substractNums(35,9);
+
