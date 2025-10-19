d5725985f7f73b09c8b399f1d923ee5aa010f4e6
BHHristov <bhristov16@gmail.com>
2025-10-19 10:23:30 +0300

    Added functions file

---
+++ functions.ts
@@ -0,0 +1,11 @@
+function displayPersonalInfo(name: string, age: number): string{
+    return `${name} is ${age} years old.`
+}
+
+console.log(displayPersonalInfo("Ivan", 34));
+
+function devideNumbers(a: number, b: number): number{
+    return a / b;
+}
+ 
+console.log(devideNumbers(10,2));
